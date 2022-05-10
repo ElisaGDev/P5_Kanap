b259d48d53babf24edd33207d675cc2670d8973e
ElisaGDev <elisa.garet@gmail.com>
2022-05-10 21:06:38 +0200

    Api repartition in DOM

--- front/html/js/script.js
+++ front/html/js/script.js
@@ -1,44 +1,39 @@
-let url = "http://localhost:3000/api/products/";
+const urlKanap = "http://localhost:3000/api/products";
 
-//Récupération des données de l'API
-fetch(url).then((res) => {
-  res
-    .json()
-    .then((data) => {
-      console.log(data);
-      articles = res;
-      console.table(articles);
-      for (let article in articles) {
-        //Insertion de l'élément <a>
-        let productLink = document.createElement("a");
-        document.querySelector(".items").appendChild(productLink);
-        productLink.href = "product.html?id=${res[article]._id}";
-        console.log(productLink);
+//Récupération des produits de l'API
 
-        //Insertion de l'élément <article>
-        let productArticle = document.createElement("article");
-        productLink.appendChild(productArticle);
+fetch(urlKanap)
+  .then((results) => {
+    return results.json();
+  })
+  .then((results) => {
+    let articles = results;
+    console.log(articles);
 
-        //Insertion de l'élément <img>
-        let productImg = document.createElement("img");
-        productArticle.appendChild(productImg);
-        productImg.src = res[article].imageUrl;
-        productImg.alt = res[article].altTxt;
-
-        //Insertion de l'élément <h3>
-        let productName = document.createElement("h3");
-        productArticle.appendChild(productName);
-        productName.classList.add("productName");
-        productName.innerHTML = res[article].name;
-
-        //Insertion de l'élément <p>
-        let productDescription = document.createElement("p");
-        productArticle.appendChild(productDescription);
-        productDescription.classList.add("productName");
-        productDescription.innerHTML = res[article].description;
-      }
-    })
-    .catch(function (error) {
-      return error;
-    });
-});
+    // Répartition des produits dans le DOM
+    console.table(articles);
+    for (let article in articles) {
+      // Insertion de l'élément "a"
+      let productLink = document.createElement("a");
+      document.querySelector(".items").appendChild(productLink);
+      productLink.href = `product.html?id=${results[article]._id}`;
+      // Insertion de l'élément "article"
+      let productArticle = document.createElement("article");
+      productLink.appendChild(productArticle);
+      // Insertion de l'image
+      let productImg = document.createElement("img");
+      productArticle.appendChild(productImg);
+      productImg.src = results[article].imageUrl;
+      productImg.alt = results[article].altTxt;
+      // Insertion du titre "h3"
+      let productName = document.createElement("h3");
+      productArticle.appendChild(productName);
+      productName.classList.add("productName");
+      productName.innerHTML = results[article].name;
+      // Insertion de la description "p"
+      let productDescription = document.createElement("p");
+      productArticle.appendChild(productDescription);
+      productDescription.classList.add("productName");
+      productDescription.innerHTML = results[article].description;
+    }
+  });
