7d4aba0be4bec48a0fe3453be5a9ecf551daa468
ElisaGDev <elisa.garet@gmail.com>
2022-05-17 01:02:13 +0200

    Creation request API product page

---
+++ front/html/js/product.js
@@ -0,0 +1,50 @@
+var str = window.location.href;
+var url = new URL(str);
+var productId = url.searchParams.get("id");
+console.log(productId);
+const urlKanap = "http://localhost:3000/api/products/";
+
+fetch(urlKanap + productId)
+  .then((results) => {
+    return results.json();
+  })
+  .then((results) => {
+    let articles = results;
+    console.log(articles);
+    if (articles) {
+      getArticle(articles);
+    }
+  })
+  .catch((error) => {
+    console.log("Erreur de la requête API");
+  });
+
+function getArticle(articles) {
+  //Ajout de l'image
+  let productImg = document.createElement("img");
+  document.querySelector(".item__img").appendChild(productImg);
+  productImg.src = articles.imageUrl;
+  productImg.alt = articles.altTxt;
+
+  // Modification du titre "h1"
+  let productName = document.getElementById("title");
+  productName.innerHTML = articles.name;
+
+  // Modification du prix
+  let productPrice = document.getElementById("price");
+  productPrice.innerHTML = articles.price;
+
+  // Modification de la description
+  let productDescription = document.getElementById("description");
+  productDescription.innerHTML = articles.description;
+
+  // Ajout des options de couleurs
+  for (let colors of articles.colors) {
+    console.table(colors);
+    let productColors = document.createElement("option");
+    document.querySelector("#colors").appendChild(productColors);
+    productColors.value = colors;
+    productColors.innerHTML = colors;
+  }
+  addToCart(articles);
+}
